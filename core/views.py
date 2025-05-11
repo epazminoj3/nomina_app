22486eaa5e03947531ce8a92ee21e5cdf3ec1ed6
epazminoj3 <epazminoj3@unemi.edu.ec>
2025-05-10 21:09:17 -0500

    Se crearon cruds de cargo

--- core/views.py
+++ core/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
+from django.shortcuts import redirect
 from core.models import Cargo
-####from core.forms import DoctorForm
+from core.forms import CargoForm
 def home(request):
     data = {
         'title': 'App Nominas',
@@ -13,19 +14,56 @@ def home(request):
     return render(request, 'home.html', data)
 
 def cargo_list(request):
+    query = request.GET.get('q', None)
+    print(query)  # Solo para debug
+    if query:
+        cargos = Cargo.objects.filter(descripcion__icontains=query)
+    else:
+        cargos = Cargo.objects.all()
+    context = {'cargos': cargos, 'title': 'Listado de cargos'}
+    return render(request, 'cargo/list.html', context)
 
-    query=request.GET.get('q',None)
-    print(query)
-    if query: cargos=Cargo.objects.filter(name__icontains=query)
-    else: cargos=Cargo.objects.all()
-    context={'cargos':cargos,'title':'listado de cargos'}
-    return render(request,'cargo/list.html', context)
+
 
 def cargo_create(request): 
-    pass
+    context={'title':'Ingresar Cargo'}
+    print("metodo: ",request.method)
+    if request.method == "GET":
+        # print("entro por get")
+
+        form = CargoForm()# instancia el formulario con los campos vacios
+        context['form'] = form
+        return render(request, 'cargo/create.html', context)
+    else:
+        #  print("entro por post")
+        form = CargoForm(request.POST) # instancia el formulario con los datos del post
+        if form.is_valid():
+            form.save()
+            # cargo = form.save(commit=False)# lo tiene en memoria
+            # cargo.user = request.user
+            # cargo.save() # lo guarda en la BD
+            return redirect('core:cargo_list')
+        else:
+            context['form'] = form
+            return render(request, 'cargo/create.html',context)
 
 def cargo_update(request,id):
-    pass
+   context={'title':'Actualizar Cargo'}
+   cargo = Cargo.objects.get(pk=id)
+   if request.method == "GET":
+      form = CargoForm(instance=cargo)# instancia el formulario con los datos del cargo
+      context['form'] = form
+      return render(request, 'cargo/create.html', context)
+   else:
+      form = CargoForm(request.POST,instance=cargo)
+      if form.is_valid():
+          form.save()
+          return redirect('core:cargo_list')
+      else:
+          context['form'] = form
+          return render(request, 'cargo/create.html', context)
 
 def cargo_delete(request,id):
-    pass
+    cargo = Cargo.objects.get(pk=id)
+    cargo.delete()
+    return redirect('core:cargo_list')
